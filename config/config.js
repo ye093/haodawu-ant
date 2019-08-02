0b3fcf6947c1c310960f8398822a0e383cd91451
yejy <ye_093@163.com>
2019-08-02 21:15:48 +0800

    完善

--- config/config.js
+++ config/config.js
@@ -28,11 +28,11 @@ const plugins = [
       },
       pwa: pwa
         ? {
-          workboxPluginMode: 'InjectManifest',
-          workboxOptions: {
-            importWorkboxFrom: 'local',
-          },
-        }
+            workboxPluginMode: 'InjectManifest',
+            workboxOptions: {
+              importWorkboxFrom: 'local',
+            },
+          }
         : false, // default close dll, because issue https://github.com/ant-design/ant-design-pro/issues/4665
       // dll features https://webpack.js.org/plugins/dll-plugin/
       // dll: {
@@ -90,7 +90,7 @@ export default {
               path: '/system/user',
               name: 'user',
               component: './system/user',
-            }
+            },
           ],
         },
         {
